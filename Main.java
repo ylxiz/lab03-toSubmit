7e2f9aa125e0539ce47ceaba9efe328987ce6f07
ylxiz <3zo0ozy119@gmail.com>
2024-09-17 20:53:19 +0300

    in this commit I have added the method that reads a list of student names and and IDs and GPAs

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
@@ -7,5 +9,22 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+        List<String> studentList = getStudents();
+        
+        for (String student : studentList) {
+            System.out.println(student);
+        }
     }
+    
+
+
+    public static List<String> getStudents() {
+        List<String> students = new ArrayList<>();
+        students.add("Abdulaziz, 1234, 3.8");
+        students.add("Abdullah, 2345, 3.6");
+        students.add("Mohammed, 3456, 3.9");
+        
+        return students;
+    }
+    
 }
